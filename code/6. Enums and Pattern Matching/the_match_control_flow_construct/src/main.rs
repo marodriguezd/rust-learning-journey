6aeb61109ae969c217b6d3c12df84fc7c1a40dc4
marodriguezd <migueadali@gmail.com>
2024-05-16 12:28:48 +0200

    6.2 done

--- code/6. Enums and Pattern Matching/the_match_control_flow_construct/src/main.rs
+++ code/6. Enums and Pattern Matching/the_match_control_flow_construct/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     patterns_that_bind_to_values();
 
     matching_with_option();
+
+    catch_all_patterns_and_the_placeholder();
 }
 
 fn example_of_coins_in_a_match() {
@@ -74,4 +76,53 @@ fn matching_with_option() {
     let five = Some(5);
     let six = plus_one(five);
     let none = plus_one(None);
+}
+
+fn catch_all_patterns_and_the_placeholder() {
+    /* This function demonstrates using match with enums to handle specific values with special
+     * actions. In this game example, rolling a 3 gives the player a fancy hat, rolling a 7 removes
+     * a fancy hat, and for all other values, the player moves that number of spaces on the game
+     * board.
+     */
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        // The catch-all pattern (_) ensures match is exhaustive and must be the last arm.
+        other => move_player(other),
+    }
+
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
+    fn move_player(num_spaces: u8) {}
+
+    // Updated game rule: roll again if you get anything other than a 3 or 7 using _ instead of
+    // a variable.
+    fn dice_rerolled() {
+        let dice_roll = 9;
+        match dice_roll {
+            3 => add_fancy_hat(),
+            7 => remove_fancy_hat(),
+            _ => reroll(),
+        }
+
+        // add_fancy_hat() and remove_fancy_hat() taken from above
+        fn reroll() {}
+    } // This example is exhaustive as the last arm ignores all other values.
+
+    // Final rule: if you roll anything other than a 3 or 7, nothing happens using the unit
+    // value ().
+    fn only_dice_3_or_7() {
+        let dice_roll = 9;
+        match dice_roll {
+            3 => add_fancy_hat(),
+            7 => remove_fancy_hat(),
+            _ => (),
+        }
+
+        // add_fancy_hat() and remove_fancy_hat() taken from above
+
+    }  // Here, we’re telling Rust explicitly that we aren’t going to use any other value that
+       // doesn’t match a pattern in an earlier arm, and we don’t want to run any code in this
+       // case.
 }
