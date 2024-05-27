c7bec0f55ef73de1fde7c5e668fcc29cd351f744
marodriguezd <migueadali@gmail.com>
2024-05-27 12:23:47 +0200

    Passing the mid point in lesson 8.1

---
+++ code/8. Common Collections/storing_lists_of_values_with_vectors/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    creating_new_vector();
+
+    updating_a_vector();
+
+    reading_elements_of_vectors();
+}
+
+fn creating_new_vector() {
+    // An empty vector
+    let v: Vec<i32> = Vec::new();
+
+    // Use the vec! macro to create a new Vec<T> auto-inferring the type.
+    // And has the new() implicit.
+    let v = vec![1, 2, 3];
+
+}
+
+fn updating_a_vector() {
+    // To add elements into a vector we use push
+    let mut v = Vec::new();  // We need a mutable one, obvious.
+
+    // After the first push auto-infer the type.
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+}
+
+fn reading_elements_of_vectors() {
+    // Reference a value in a vector using indexing or the get method.
+    // Both return a reference, but get returns an Option for safe access.
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
+}
