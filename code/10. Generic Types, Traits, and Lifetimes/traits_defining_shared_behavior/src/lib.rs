d5eb77c9d5da6d952d890dfc3af6a30009fa1fe2
marodriguezd <migueadali@gmail.com>
2024-07-16 11:20:15 +0200

    feat: Implement Summary trait with default method and use in main - Added a default implementation for the Summary trait. - Demonstrated the use of the default implementation with the NewsArticle struct. - Included example usage in main to show both a Tweet and a NewsArticle summary.

--- code/10. Generic Types, Traits, and Lifetimes/traits_defining_shared_behavior/src/lib.rs
+++ code/10. Generic Types, Traits, and Lifetimes/traits_defining_shared_behavior/src/lib.rs
@@ -1,12 +1,26 @@
 /// The `Summary` trait defines a shared behavior for summarizing the content of a type.
 /// Any type that implements this trait must define the `summarize` method.
 pub trait Summary {
+    // V1
+    /*
     /// Returns a summary of the content.
     ///
     /// Each type implementing this trait must provide its own custom implementation of the method.
     /// The compiler ensures that any type with the `Summary` trait has the `summarize` method
     /// defined with this exact signature.
     fn summarize(&self) -> String;
+    */
+
+    // V2
+    /// Returns a summary of the content.
+    ///
+    /// This method has a default implementation that returns a generic summary string.
+    /// Types implementing this trait can override this method to provide a custom summary.
+    /// The compiler ensures that any type with the `Summary`trait has the `summarize` method
+    /// defined with this exact signature.
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
 }
 
 
@@ -18,6 +32,8 @@ pub struct NewsArticle {
     pub content: String,
 }
 
+// V1
+/*
 /// Implementing the `Summary` trait for `NewsArticle`.
 ///
 /// The `summarize` method returns a string that summarizes the article using its headline, author, and location.
@@ -26,6 +42,17 @@ impl Summary for NewsArticle {
         format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
 }
+*/
+
+// V2
+/// Implementing the `Summary` trait for `NewsArticle` with default implementation.
+///
+/// To use a default implementation to summarize instances of `NewsArticle`.
+/// we specify an empty impl block with `impl Summary for NewsArticle {}`.
+/// Even though we're no longer defining the summarize method on `NewsArticle` directly,
+/// we've provided a default implementation and specified that `NewsArticle` implements
+/// the `Summary` trait. As a result, we can still call the summarize method on an instance of `NewsArticle`.
+impl Summary for NewsArticle {}
 
 /// A `Tweet` struct representing a tweet with a username, content, and metadata about replies and retweets.
 pub struct Tweet {
